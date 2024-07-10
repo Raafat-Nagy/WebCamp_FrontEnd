9896d9557ed1faaff770ac816e1cff5c860ceaad
Raafat-Nagy <Raafatnagy89@gmail.com>
2024-07-10 15:15:10 +0300

    Solution Of Task 'Introduction (001-009)'

---
+++ 3.JavaScript/01.Elzero_Assignments/01.Introduction-(001-009)/Assignments-5/main.js
@@ -0,0 +1,1 @@
+console.table(["Value-1", "Value-2", "Value-3", "Value-4", "Value-5"]);
---
+++ 3.JavaScript/01.Elzero_Assignments/01.Introduction-(001-009)/Assignments-6/main.js
@@ -0,0 +1,8 @@
+// First method "single-line comments"
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+/* Second method "multi-line comments" 
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
